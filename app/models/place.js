64b4318ada602b4cc0bf7e3b9bcc2d2a3e303372
Aaron Saunders <aaronksaunders@gmail.com>
2012-09-26 14:41:37 -0400

    updated model code to support latest alloy release

--- app/models/place.js
+++ app/models/place.js
@@ -1,9 +1,35 @@
-(function(Model) {
-	// add code to modify/extend your Model definition
-	// Example:
-	//   return Model.extend({
-	//       customProperty: 123,
-	//       customFunction: function() {}
-	//   });
-	return Model; 
-})
+exports.definition = {
+
+    config : {
+        "columns" : {
+        },
+        "defaults" : {},
+        "adapter" : {
+            "type" : "acs",
+            "collection_name" : "places"
+        },
+        "settings" : {
+            "object_name" : "places",
+            "object_method" : "Places"
+        }
+    },
+
+    extendModel : function(Model) {
+        _.extend(Model.prototype, {
+
+
+        });
+        // end extend
+
+        return Model;
+    },
+
+    extendCollection : function(Collection) {
+        _.extend(Collection.prototype, {
+
+        });
+        // end extend
+
+        return Collection;
+    }
+}
--- app/models/user.js
+++ app/models/user.js
@@ -1,65 +1,86 @@
-(function(Model) {
-	// add code to modify/extend your Model definition
-	// Example:
-	//   return Model.extend({
-	//       customProperty: 123,
-	//       customFunction: function() {}
-	//   });
+exports.definition = {
 
-	function login(_login, _password, _opts) {
-		var self = this;
-		this.config.Cloud.Users.login({
-			login : _login,
-			password : _password
-		}, function(e) {
-			if (e.success) {
-				var user = e.users[0];
-				Ti.API.info('Logged in! You are now logged in as ' + user.id);
-				_opts.success && _opts.success(new model(user));
-			} else {
-				Ti.API.error(e);
-				_opts.error && _opts.error((e.error && e.message) || e);
-			}
-		});
-	}
+    config : {
+        "columns" : {
+            "active" : "boolean"
+        },
+        "adapter" : {
+            "type" : "acs",
+            "collection_name" : "users"
+        },
+        "settings" : {
+            "object_name" : "users",
+            "object_method" : "Users"
+        }
+    },
+    extendModel : function(Model) {
 
-	function hasStoredSession() {
-		return this.config.Cloud.hasStoredSession();
-	}
+        function logout(_opts) {
+            this.config.Cloud.Users.logout(function(e) {
+                if (e.success) {
+                    _opts.success && _opts.success(null);
+                } else {
+                    _opts.error && _opts.error((e.error && e.message) || e);
+                }
+            });
+        }
 
-	function retrieveStoredSession() {
-		return this.config.Cloud.retrieveStoredSession();
-	}
+        function showMe(_opts) {
+            this.config.Cloud.Users.showMe(function(e) {
+                if (e.success) {
+                    var user = e.users[0];
+                    Ti.API.info('Logged in! You are now logged in as ' + user.id);
+                    _opts.success && _opts.success(new model(user));
+                } else {
+                    Ti.API.error(e);
+                    _opts.error && _opts.error((e.error && e.message) || e);
+                }
+            });
+        }
 
-	function logout(_opts) {
-		this.config.Cloud.Users.logout(function(e) {
-			if (e.success) {
-				_opts.success && _opts.success(null);
-			} else {
-				_opts.error && _opts.error((e.error && e.message) || e);
-			}
-		});
-	}
+        function hasStoredSession() {
+            return this.config.Cloud.hasStoredSession();
+        }
 
-	function showMe(_opts) {
-		this.config.Cloud.Users.showMe(function(e) {
-			if (e.success) {
-				var user = e.users[0];
-				Ti.API.info('Logged in! You are now logged in as ' + user.id);
-				_opts.success && _opts.success(new model(user));
-			} else {
-				Ti.API.error(e);
-				_opts.error && _opts.error((e.error && e.message) || e);
-			}
-		});
-	}
+        function retrieveStoredSession() {
+            return this.config.Cloud.retrieveStoredSession();
+        }
 
-	return Model.extend({
-		login : login,
-		showMe : showMe,
-		logout : logout,
-		retrieveStoredSession : retrieveStoredSession,
-		hasStoredSession : hasStoredSession,
+        function login(_login, _password, _opts) {
+            var self = this;
+            this.config.Cloud.Users.login({
+                login : _login,
+                password : _password
+            }, function(e) {
+                if (e.success) {
+                    var user = e.users[0];
+                    Ti.API.info('Logged in! You are now logged in as ' + user.id);
+                    _opts.success && _opts.success(new model(user));
+                } else {
+                    Ti.API.error(e);
+                    _opts.error && _opts.error((e.error && e.message) || e);
+                }
+            });
+        }
 
-	});
-})
+
+        _.extend(Model.prototype, {
+            login : login,
+            showMe : showMe,
+            logout : logout,
+            retrieveStoredSession : retrieveStoredSession,
+            hasStoredSession : hasStoredSession,
+        });
+        // end extend
+
+        return Model;
+    },
+    extendCollection : function(Collection) {
+        _.extend(Collection.prototype, {
+
+        });
+        // end extend
+
+        return Collection;
+    }
+}
